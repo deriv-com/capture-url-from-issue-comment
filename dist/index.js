cefe0e75761f9a1172cb19d4b06302be54dca564
balakrishna-deriv <56330681+balakrishna-deriv@users.noreply.github.com>
2023-11-03 16:16:13 +0800

    chore: fix import issue (#5)

--- dist/index.js
+++ dist/index.js
@@ -30008,6 +30008,29 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -30017,17 +30040,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 const action_1 = __nccwpck_require__(1231);
 const cancelAction = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (core_1.default.getInput('GITHUB_TOKEN')) {
+    if (core.getInput('GITHUB_TOKEN')) {
         const octokit = new action_1.Octokit();
-        yield octokit.actions.cancelWorkflowRun(Object.assign(Object.assign({}, github_1.default.context.repo), { run_id: github_1.default.context.runId }));
+        yield octokit.actions.cancelWorkflowRun(Object.assign(Object.assign({}, github.context.repo), { run_id: github.context.runId }));
         // Wait a maximum of 1 minute for the action to be cancelled.
         yield new Promise(resolve => setTimeout(resolve, 60000));
     }
@@ -30035,21 +30055,21 @@ const cancelAction = () => __awaiter(void 0, void 0, void 0, function* () {
     process.exit(1);
 });
 const runAction = () => __awaiter(void 0, void 0, void 0, function* () {
-    const { payload } = github_1.default.context;
+    const { payload } = github.context;
     const { comment } = payload;
     if (!comment) {
         console.log('Action triggered on non-comment event.');
         yield cancelAction();
         return;
     }
-    const bot_name = core_1.default.getInput('bot_name');
+    const bot_name = core.getInput('bot_name');
     if (comment.user.login !== bot_name) {
         console.log('Comment did not originate from bot.', {
             bot_name,
         });
         yield cancelAction();
     }
-    const cancel_pattern = core_1.default.getInput('cancel_pattern');
+    const cancel_pattern = core.getInput('cancel_pattern');
     if (cancel_pattern && new RegExp(cancel_pattern).test(comment.body)) {
         console.log('Comment contained pattern that should cancel the action.', {
             cancel_pattern,
@@ -30057,7 +30077,7 @@ const runAction = () => __awaiter(void 0, void 0, void 0, function* () {
         });
         yield cancelAction();
     }
-    const url_pattern = new RegExp(core_1.default.getInput('url_pattern'));
+    const url_pattern = new RegExp(core.getInput('url_pattern'));
     const regex_matches = comment.body.match(url_pattern);
     if (!regex_matches) {
         console.log("Unable to find a URL in comment's body.", {
@@ -30068,7 +30088,7 @@ const runAction = () => __awaiter(void 0, void 0, void 0, function* () {
     const url = regex_matches[1];
     if (url) {
         console.log('Found URL.', { url });
-        core_1.default.setOutput('url', url);
+        core.setOutput('url', url);
         process.exit(0);
     }
     else {
